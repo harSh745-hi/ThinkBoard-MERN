44acc6d299244d009616a6d95482debe9ecfea41
harSH745-hi <harshpandharpatte@gmail.com>
2025-12-03 14:43:53 +0530

    ready to deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 
 import notesRoutes from "./routes/noteRoutes.js";
@@ -11,21 +12,35 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 // connect db
 
 // middleware
-app.use(cors(
+if(process.env.NODE_ENV !== "production"){
+
+  app.use(cors(
   {
     origin:"http://localhost:5173"
   }
 ));
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 
 // routes
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+});
+
+}
 
 app.get('/',(req,res)=>{
   res.json({message:"hello"});
