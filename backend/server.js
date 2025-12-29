817dc19005d7ec13669bcd927fe167d0ad6d0939
harSh745-hi <harshpandharpatte@gmail.com>
2025-12-29 10:58:47 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,56 +2,38 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
-
-
 import notesRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve()
-
-// connect db
+const __dirname = path.resolve();
 
 // middleware
 if(process.env.NODE_ENV !== "production"){
-
-  app.use(cors(
-  {
-    origin:"http://localhost:5173"
-  }
-));
+  app.use(cors({
+    origin: "http://localhost:5173"
+  }));
 }
 
 app.use(express.json());
 app.use(rateLimiter);
 
-
 // routes
 app.use("/api/notes", notesRoutes);
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
-
-app.get("*", (req,res) => {
-  res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-});
-
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
-app.get('/',(req,res)=>{
-  res.json({message:"hello"});
-}
-);
-
-connectDB().then( ()=>{
-    app.listen(PORT, () => {
-    console.log(`server started on ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server started on ${PORT}`);
+  });
 });
-
-})
-
-
